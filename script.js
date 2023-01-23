3d6a7d3a50f5ba3c5739d35f7fd45f010aff32a0
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-01-23 04:24:41 -0500

    Added smooth scrolling with js file

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+function scrollTo(element) {
+  window.scroll({
+    behavior: "smooth",
+    left: 0,
+    top: element.offsetTop,
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    const element = document.querySelector(href);
+    setTimeout(() => scrollTo(element), 3000);
+  });
+});
