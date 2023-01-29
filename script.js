c0a0d2a9b634578ff9472456964b63d3663ee37b
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-01-29 14:13:18 -0500

    Removed google captcha

--- script.js
+++ script.js
@@ -52,3 +52,14 @@ document.querySelector("form").addEventListener("submit", handleSubmit);
 function onSubmit(token) {
   document.getElementById("contact-form").submit();
 }
+
+exports.handler = async function (event, context) {
+  const value = process.env.SITE_KEY;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Value of MY_IMPORTANT_VARIABLE is ${value}.`,
+    }),
+  };
+};
