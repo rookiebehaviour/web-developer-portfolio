d4dffd17d55b437e4baa5e0995624a8524332dd2
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-01-29 04:45:30 -0500

    Added form and recaptcha

--- script.js
+++ script.js
@@ -27,3 +27,24 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     setTimeout(() => scrollTo(element), 3000);
   });
 });
+
+// Form Submissions - Netlify
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+// Hnadle callback token - Google recaptcha
