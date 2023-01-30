e2310bf411f6d7b3d91b0d40303f8e4a7d23c62c
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-01-29 20:48:46 -0500

    Added script to head

--- script.js
+++ script.js
@@ -23,6 +23,43 @@ document.querySelectorAll("a[href^='#']").forEach((anchor) => {
 
 // Form submission - Formspree
 
+var form = document.getElementById("contact-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("contact-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerText = "Your message is on it's way to Jacqueline!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerText = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerText =
+              "Oops! There was a problem sending your message, please retry.";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerText =
+        "Oops! There was a problem sending your message, please retry.";
+    });
+}
+form.addEventListener("submit", handleSubmit);
+
 window.onbeforeunload = () => {
   for (const form of document.getElementsByTagName("form")) {
     form.reset();
