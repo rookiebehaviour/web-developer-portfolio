9ffaaa03f276068d63ea2224f87e2aba7d3e75a8
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-02-06 11:40:44 -0500

    Updated event listener

--- script.js
+++ script.js
@@ -18,41 +18,42 @@ document.querySelectorAll("a[href^='#']").forEach((anchor) => {
 });
 
 // Form submission - Formspree
+document.addEventListener("DOMContentLoaded", function () {
+  let form = document.getElementById("contact-form");
 
-let form = document.getElementById("contact-form");
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  let name = event.target.name.value;
-  let status = document.getElementById("contact-form-status");
-  let data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        status.innerText = `Thanks ${name}, your message was sent to Jacqueline!`;
-        form.reset();
-      } else {
-        response.json().then((data) => {
-          if (Object.hasOwn(data, "errors")) {
-            status.innerText = data["errors"]
-              .map((error) => error["message"])
-              .join(", ");
-          } else {
-            status.innerText =
-              "Oops! There was a problem submitting your message, please retry.";
-          }
-        });
-      }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    let name = event.target.name.value;
+    let status = document.getElementById("contact-form-status");
+    let data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        Accept: "application/json",
+      },
     })
-    .catch((error) => {
-      status.innerText =
-        "Oops! There was a problem submitting your message, please retry.";
-    });
-}
+      .then((response) => {
+        if (response.ok) {
+          status.innerText = `Thanks ${name}, your message was sent to Jacqueline!`;
+          form.reset();
+        } else {
+          response.json().then((data) => {
+            if (Object.hasOwn(data, "errors")) {
+              status.innerText = data["errors"]
+                .map((error) => error["message"])
+                .join(", ");
+            } else {
+              status.innerText =
+                "Oops! There was a problem submitting your message, please retry.";
+            }
+          });
+        }
+      })
+      .catch((error) => {
+        status.innerText =
+          "Oops! There was a problem submitting your message, please retry.";
+      });
+  }
+});
 form.addEventListener("submit", handleSubmit);
